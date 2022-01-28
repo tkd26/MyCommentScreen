185292529a20059471cebec7318845e63363d714
tkd26 <takedam0226@gmail.com>
2022-01-29 00:09:51 +0900

    first commit

---
+++ public/visitor.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const topMenu = document.getElementById("topMenu");
+const msgForm = document.getElementById("msgForm");
+const exitForm = document.getElementById("exitForm");
+const roomIdText = document.getElementById('roomIdText');
+const roomIdArea = document.getElementById('roomIdArea');
+
+function toTopMenu () {
+    topMenu.style.display ="block";
+    msgForm.style.display ="none";
+    exitForm.style.display ="none";
+    roomIdArea.style.display ="none";
+}
+
+function toRoom () {
+    topMenu.style.display ="none";
+    msgForm.style.display ="block";
+    exitForm.style.display ="block";
+    roomIdArea.style.display ="block";
+}
+
+if (sessionStorage.getItem('roomId')) {
+    toRoom();
+    roomIdText.innerHTML = sessionStorage.getItem('roomId');
+} else {
+    toTopMenu();
+}
+
+// クライアントからサーバーへの接続要求
+const socket = io.connect();
+
+// 接続時の処理
+socket.on('connect', () => {
+    console.log('connect');
+
+    if (sessionStorage.getItem('roomId')) {
+        socket.emit('roomId', sessionStorage.getItem('roomId'));
+    }
+});
+
+// room IDの入力を受け付けるフォームでの処理
+$('#topMenu').submit(() => {
+    const $inp = $('#inputRoomId');
+    const roomId = $inp.val();
+
+    if (roomId) {
+        sessionStorage.setItem('roomId', roomId);
+
+        // room IDをサーバへ送ることで，roomへ入室する
+        socket.emit('roomId', sessionStorage.getItem('roomId'));
+
+        $inp.val('');
+
+        toRoom();
+    
+        roomIdText.innerHTML = sessionStorage.getItem('roomId');
+    }
+
+    // フォーム送信はしない
+    return false;
+
+});
+
+// メッセージを送信するフォームでの処理
+$('#msgForm').submit(() => {
+    const $inp = $('#input_message');
+    const text = $inp.val();
+
+    if (text) {
+        // サーバーに、イベント名 'new message' で入力テキストを送信
+        // これは参加したroom ID内にのみ送信される
+        socket.emit('new message', text);
+        // テキストボックスを空に
+        $inp.val('');
+    }
+    // フォーム送信はしない
+    return false;
+});
+
+// 退出ボタンを押した時の処理
+$('#exitForm').submit(() => {
+    sessionStorage.removeItem('roomId');
+    toTopMenu();
+});
---
+++ server.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// モジュール
+const http     = require('http');
+const express  = require('express');
+const socketIO = require('socket.io');
+// const moment   = require('moment');
+var fs = require('fs');
+
+// オブジェクト
+const app    = express();
+const server = http.Server(app);
+const io     = socketIO(server);
+
+// 定数
+const PORT = process.env.PORT || 3000;
+
+// 接続時の処理
+// 'connection'はフロント側と接続されている確認するときに使う
+// socketには送信されたメッセージが格納されている
+io.on('connection', (socket) => {
+    console.log('connection');
+
+    // 切断時の処理
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+    });
+
+    // room IDを受け取った時の処理
+    socket.on('roomId', (roomid) => {
+        // 特定のroomに入室させる
+        socket.join(roomid);
+
+        // 新しいメッセージ受信時の処理
+        socket.on('new message', (strMessage) => {
+            // console.log('new message:', roomid, strMessage);
+            // 受け取ったroom IDのroomにのみメッセージを送信する
+            io.to(roomid).emit('message', strMessage);
+        });
+    });
+
+});
+
+// 公開フォルダの指定
+app.use(express.static(__dirname + '/public'));
+
+// サーバーの起動
+server.listen(PORT, () => {
+    console.log('server starts on port: %d', PORT);
+});
